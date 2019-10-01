958234b91cf48daf56d5d755650b0452f453086d
Alexcor43 <alexcordev@gmail.com>
2019-10-01 07:57:33 -0400

    On Scroll Event working and menu on process

--- js/main.js
+++ js/main.js
@@ -7,10 +7,20 @@ $(document).ready( function() {
     $('#main').slideDown( 1000, function() {
       $( this )
         .css( "border", "2px #0000ff inset" )
-        $('#joindre').css('display', 'none');
+        
     });
 
       });
+      $('#joindre').on('dblclick', function(){
+        $('#main').slideUp( 1000, function() {
+          $( this )
+            .css( "border", "2px #0000ff inset" )
+            $('#joindre').show();
+        });
+    
+          });
+    
+     
   
       $('#joindre').on('dblclick', function(){
         $('#form').hide();
@@ -62,4 +72,18 @@ $(document).ready( function() {
         var nom = $('input#nom').val();
         alert("J'ai bien reçu votre message " + nom + " Merci !");
       });
+
+      
+$(window).bind("scroll", function(event) { // this checks if the scroll event is occurs
+	    $("#progress:in-viewport").each(function() { // this is the selector for viewport. check the link above for more info
+      $('#bar1').barfiller({barColor: "#33cc33", duration: 2000});
+      $('#bar2').barfiller({barColor: "#ffa31a", duration: 2400});
+      $('#bar3').barfiller({barColor: "#b8b894", duration: 2800});
+      $('#bar4').barfiller({barColor: "#66e0ff", duration: 3200});
+      $('#bar5').barfiller({barColor: "#ff5c33", duration: 3600});
+      $('#bar6').barfiller({barColor: "#ffdb4d", duration: 4000});
+      $('#bar7').barfiller({barColor: "#33cc33", duration: 4400});
+    });  
 });
+});
+  
